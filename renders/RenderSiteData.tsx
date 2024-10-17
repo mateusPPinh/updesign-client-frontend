af4c97b7744736430ca6ac2e6ae9b0fc26c4c2a0
Mateus <rawrenete@gmail.com>
2024-10-17 09:40:13 -0300

    mock menu in ISR

--- renders/RenderSiteData.tsx
+++ renders/RenderSiteData.tsx
@@ -3,6 +3,7 @@ import { ArticleProps } from '@app/utils/shared-interfaces';
 import dynamic from 'next/dynamic';
 import get from 'lodash/get';
 import { colorClassMap } from '@app/styles/colorClassMap';
+import { menu } from '@app/utils/redundancy/menu'
 
 const Footer = dynamic(() => import('@app/components/Struct/Footer'), {
   ssr: false
@@ -20,9 +21,6 @@ interface Props {
 }
 
 export default function RenderSiteData({ articleData, components }: Props) {
-  const deliveryMenuData = get(components, '[0]', []);
-  const menuData = get(deliveryMenuData, 'menu', []);
-
   const bgColorClass =
     typeof articleData.pageBgColor === 'string' &&
     colorClassMap[articleData.pageBgColor]
@@ -31,9 +29,9 @@ export default function RenderSiteData({ articleData, components }: Props) {
   return (
     <div className={`${bgColorClass} min-h-screen`}>
       <div
-        className={`${menuData.length > 0 ? 'w-full mb-10 pb-0 overflow-hidden' : 'hidden'}`}
+        className='w-full mb-10 pb-0 overflow-hidden'
       >
-        <DynamicMenu items={menuData} />
+        <DynamicMenu items={menu} />
         <Article
           key={articleData.slug}
           articleBody={articleData.articleBody}
