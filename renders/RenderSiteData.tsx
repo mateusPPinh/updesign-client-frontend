f97fb0148a90d5a5cdf192f2efb0eeddc5165d89
Mateus <rawrenete@gmail.com>
2024-10-17 09:41:01 -0300

    fix import

--- renders/RenderSiteData.tsx
+++ renders/RenderSiteData.tsx
@@ -3,7 +3,7 @@ import { ArticleProps } from '@app/utils/shared-interfaces';
 import dynamic from 'next/dynamic';
 import get from 'lodash/get';
 import { colorClassMap } from '@app/styles/colorClassMap';
-import { menu } from '@app/utils/redundancy/menu'
+import menu from '@app/utils/redundancy/menu'
 
 const Footer = dynamic(() => import('@app/components/Struct/Footer'), {
   ssr: false
